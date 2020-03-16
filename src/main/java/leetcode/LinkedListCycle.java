88b765be0fdad839624e097614cb78eb008e4891
jifeng.sun <sjf_159@outlook.com>
2020-03-16 10:01:56 +0800

    添加递进式解法

--- src/main/java/leetcode/LinkedListCycle.java
+++ src/main/java/leetcode/LinkedListCycle.java
@@ -2,6 +2,10 @@ package leetcode;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * 给定一个链表，判断链表中是否有环。
@@ -9,14 +13,37 @@ import java.util.Set;
  * 进阶：
  * 你能用 O(1)（即，常量）内存解决此问题吗？
  *
+ * 面试时不要一上来就给出最最优解
+ * 1. 等待
+ * 2. Set
+ * 3. 快慢指针
+ *
  * @author 孙继峰
  * @date 2019/08/27
  */
 public class LinkedListCycle {
+
+    public boolean hasCycle1(ListNode head) {
+        FutureTask<Boolean> task = new FutureTask<>(() -> {
+            ListNode h = head;
+            while (h != null) {
+                h = h.next;
+            }
+            return true;
+        });
+
+        try {
+            task.get(500, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            return true;
+        }
+        return false;
+    }
+
     /**
-     * 下策
+     * Set
      */
-    public boolean hasCycle(ListNode head) {
+    public boolean hasCycle2(ListNode head) {
         Set<ListNode> set = new HashSet<>();
         while (head != null) {
             if (!set.add(head)) {
@@ -30,7 +57,7 @@ public class LinkedListCycle {
     /**
      * 快慢指针
      */
-    public boolean hasCycle2(ListNode head) {
+    public boolean hasCycle3(ListNode head) {
         if (head == null || head.next == null) {
             return false;
         }
