818d83a33368f48b59ec8db73df0f808ff898b6d
孙继峰 <sun.jifeng@outlook.com>
2020-04-15 23:18:15 +0800

    AddTwoNumbersII 优化

--- src/main/java/leetcode/AddTwoNumbersII.java
+++ src/main/java/leetcode/AddTwoNumbersII.java
@@ -12,49 +12,33 @@ import java.util.Stack;
  */
 public class AddTwoNumbersII {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<ListNode> stack1 = new Stack<>();
-        Stack<ListNode> stack2 = new Stack<>();
+        // 使用 Stack 将链表逆序
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
         while (l1 != null) {
-            stack1.push(l1);
+            stack1.push(l1.val);
             l1 = l1.next;
         }
         while (l2 != null) {
-            stack2.push(l2);
+            stack2.push(l2.val);
             l2 = l2.next;
         }
 
         ListNode dummyHead = new ListNode(0);
-        boolean add = false;
-        while (!stack1.isEmpty() || !stack2.isEmpty() || add) {
-            if (!stack1.isEmpty() && !stack2.isEmpty()) {
-                int sum = stack1.pop().val + stack2.pop().val;
-                if (add) {
-                    sum++;
-                    add = false;
-                }
-                if (sum >= 10) {
-                    sum -= 10;
-                    add = true;
-                }
-                ListNode listNode = new ListNode(sum);
-                listNode.next = dummyHead.next;
-                dummyHead.next = listNode;
-            } else if (!stack1.isEmpty()) {
-                ListNode listNode = new ListNode(stack1.pop().val);
-                listNode.next = dummyHead.next;
-                dummyHead.next = listNode;
-            } else if (!stack2.isEmpty()){
-                ListNode listNode = new ListNode(stack2.pop().val);
-                listNode.next = dummyHead.next;
-                dummyHead.next = listNode;
+        int flag = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || flag != 0) {
+            int sum = (stack1.isEmpty() ? 0 : stack1.pop()) + (stack2.isEmpty() ? 0 : stack2.pop()) + flag;
+            if (sum >= 10) {
+                sum -= 10;
+                flag = 1;
             } else {
-                ListNode listNode = new ListNode(1);
-                listNode.next = dummyHead.next;
-                dummyHead.next = listNode;
-                add = false;
+                flag = 0;
             }
+            // 头插法再逆序
+            ListNode node = new ListNode(sum);
+            node.next = dummyHead.next;
+            dummyHead.next = node;
         }
-
         return dummyHead.next;
     }
 }
