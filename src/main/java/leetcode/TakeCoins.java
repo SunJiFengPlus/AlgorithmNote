4e3fda7634556648fd0ade9667ba9f6196f7b6c3
孙继峰 <sun.jifeng@outlook.com>
2020-04-18 16:27:46 +0800

    TDD - LeetCode春季编程大赛 - TakeCoins 通过测试用例1

---
+++ src/main/java/leetcode/TakeCoins.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author 孙继峰
+ * @since 2020/4/18
+ */
+public class TakeCoins {
+    /**
+     *  https://leetcode-cn.com/contest/season/2020-spring/problems/na-ying-bi-UGC/
+     */
+    public int minCount(int[] coins) {
+        return Arrays.stream(coins).map(a -> (int) Math.ceil(a / 2D)).sum();
+    }
+
+    @Test
+    public void test() {
+        Assert.assertEquals(minCount(new int[]{4,2,1}), 4);
+    }
+}
