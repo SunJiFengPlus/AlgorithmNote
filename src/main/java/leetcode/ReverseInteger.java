5de2b7739e2493c0a2f75c0bbccb100043c2595d
孙继峰 <sun.jifeng@outlook.com>
2020-04-21 06:43:20 +0800

    TDD - ReverseInteger 通过测试用例1

--- src/main/java/leetcode/ReverseInteger.java
+++ src/main/java/leetcode/ReverseInteger.java
@@ -1,11 +1,31 @@
 package leetcode;
 
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 /**
- * https://leetcode-cn.com/problems/reverse-integer/能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31−1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * https://leetcode-cn.com/problems/reverse-integer/
  *
  * @author 孙继峰
  * @since  2020/04/21
  */
 public class ReverseInteger {
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            res *= 10;
+            res += x % 10;
+            x /= 10;
+        }
+        return res;
+    }
 
+    @ParameterizedTest
+    @CsvSource({
+            "123, 321"
+    })
+    public void test(int input, int output) {
+        Assert.assertEquals(reverse(input), output);
+    }
 }
