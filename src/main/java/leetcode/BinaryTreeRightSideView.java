6829ae17f40ca50449090dd7ca7755e83e9ef4f3
孙继峰 <sun.jifeng@outlook.com>
2020-04-21 06:43:20 +0800

    init ReverseInteger

---
+++ src/main/java/leetcode/BinaryTreeRightSideView.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import leetcode.structure.TreeNode;
+
+import java.util.*;
+
+/**
+ * https://leetcode-cn.com/problems/binary-tree-right-side-view/
+ *
+ * @author 孙继峰
+ * @since 2020/4/22
+ */
+public class BinaryTreeRightSideView {
+    /**
+     * 思路: 层序遍历取最后一个元素
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        List<TreeNode> wait = new ArrayList<>();
+        Deque<TreeNode> queue = new LinkedList<>();
+
+
+        return res;
+    }
+}
